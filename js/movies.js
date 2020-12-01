3d34d07940f80d60a16f26cafb3de7c46e0e14f9
unknown <rtdoll@k-state.edu>
2020-11-30 21:43:08 -0600

    Add functionality for 2nd user

--- js/movies.js
+++ js/movies.js
@@ -1,15 +1,23 @@
 const imgSrc = "images/";
 const jpg = ".jpg";
 let movies = ["Avatar", "Insidious", "RV", "The Adventures of Wolf Boy", "The Fault In Our Stars", "The Hobbit"];
+let user = [];
 let user1 = [];
-let user2 = [];
+let matches = [];
 let moviesIndex = 0;
+let userIndex = 0;
 let user1Index = 0;
-let user2Index = 0;
+let userFlag = 0; // 0 for user 1 for user1
 
 function likeMovie() {
-    user1.push(document.getElementById("movie-name"));
-    user1Index++;
+    if (userFlag == 0) {
+        user.push(document.getElementById("movie-name"));
+        userIndex++;
+    }
+    else {
+        user1.push(document.getElementById("movie-name"));
+        user1Index++;
+    }
     nextMovie();
 }
 
@@ -18,5 +26,40 @@ function nextMovie() {
         moviesIndex++;
         document.getElementById("movie-name").innerText = movies[moviesIndex];
         document.getElementById("movie-img").src = imgSrc.concat(movies[moviesIndex]).concat(jpg);
+    }
+    else if (moviesIndex == movies.length - 1 && userFlag == 0) {
+        setupNextUser();
+    }
+    else {
+        window.location.href = "C:/Users/Ryan/Development/CIS598-MovieMatch/partner.html"; //change to reflect actual webpage not local
+    }
+}
+
+function setupNextUser() {
+    moviesIndex = 0;
+    userFlag = 1;
+    document.getElementById("movie-img").src = "";
+    document.getElementById("movie-name").innerText = "Now for the 2nd person!";
+    document.getElementById("start").style.visibility = "visible";
+    document.getElementById("sike").style.visibility = "hidden";
+    document.getElementById("like").style.visibility = "hidden";
+}
+
+function startNextUser() {
+    document.getElementById("movie-img").src = imgSrc.concat(movies[moviesIndex]).concat(jpg);
+    document.getElementById("movie-name").innerText = movies[moviesIndex];
+    document.getElementById("start").style.visibility = "hidden";
+    document.getElementById("sike").style.visibility = "visible";
+    document.getElementById("like").style.visibility = "visible";
+}
+
+function findMatches() {
+    for (i = 0; i < user.length; i++) {
+        for (f = 0; f < user1.length; i++) {
+            if (user[i] == user1[f]) {
+                matches.push(user[i]);
+                break;
+            }
+        }
     }
 }
