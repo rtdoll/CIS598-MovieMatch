fe25e10e42e4eb872112652d20d8bd8ab3f2b23b
unknown <rtdoll@k-state.edu>
2020-12-01 20:50:59 -0600

    Add navigation back to home

--- js/partner.js
+++ js/partner.js
@@ -3,11 +3,23 @@ const matchedMovies = decodeURI(urlParams.getAll('movieMatches')).split(","); //
 
 let list = document.getElementById("movieList"); // define ul item to add movies to
 
-matchedMovies.forEach(element => addItem(element));
+window.onload = function() {
+    if (matchedMovies < 1) {
+        document.getElementById("info-text").innerText = "Oh no, you had no movies in common.\n";
+        document.getElementById("movieList").style.visibility = "hidden";
+    }
+    else {
+        matchedMovies.forEach(element => addItem(element));
+    }
+}
 
 // add each movie string to list
 function addItem(item) {
     let le = document.createElement('li');
     le.textContent = item;
     list.appendChild(le);
+}
+
+function toHome() {
+    window.location.href = "home";
 }
